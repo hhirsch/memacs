0fc0a49370f274ee4c8399bf0e353b511b8ef107
Henry Hirsch <henry@w3-net.de>
2023-12-02 11:36:22 +0100

    feat: simplify theme comments

--- init.el
+++ init.el
@@ -78,18 +78,13 @@
 (use-package doom-themes
   :ensure t
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (setq doom-themes-enable-bold t   
+        doom-themes-enable-italic t)
   (load-theme 'doom-one t)
-  ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-atom")
   (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
 (add-hook 'after-save-hook 'autocompile-init-file)
