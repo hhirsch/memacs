418cd232285ad2dac424016860619dc6fd2399dc
Henry Hirsch <henry@w3-net.de>
2023-12-02 14:56:55 +0100

    feat: better documentation

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+;;  __  __      __  __
+;; |  \/  | ___|  \/  | __ _  ___ ___
+;; | |\/| |/ _ \ |\/| |/ _` |/ __/ __|
+;; | |  | |  __/ |  | | (_| | (__\__ \
+;; |_|  |_|\___|_|  |_|\__,_|\___|___/
+;;
+;; Light Emacs Distribution
+
 (defun ensure-use-package-is-installed ()
   "Installs use package if it is not installed"
   (unless (package-installed-p 'use-package)
@@ -10,7 +18,7 @@
   (require 'bytecomp)
   (let ((dotemacs (file-truename user-init-file)))
     (if (string= (buffer-file-name) (file-chase-links dotemacs))
-      (byte-compile-file dotemacs))))
+        (byte-compile-file dotemacs))))
 
 (defun install-language-server-hooks ()
   "Automatically start lsp for certain programming languages"
@@ -21,7 +29,7 @@
     (add-hook 'c-mode-hook #'lsp)
     (add-hook 'c++-mode-hook #'lsp)
     (add-hook 'js-mode-hook #'lsp)
-  )
+    )
   )
 
 (defun setup-editor-for-programming ()
@@ -29,47 +37,47 @@
   (global-linum-mode 1)
   (ido-mode 1)
   (set-face-attribute 'default nil :height 150)
-  ; indentation
+  ;; indentation
   (setq-default indent-tabs-mode nil)
   (setq-default tab-width 2)
   (setq indent-line-function 'insert-tab)
- )
+  )
 
 (require 'package) ; load package managment
-; add sources to load our packages from
+;; add sources to load our packages from
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize) ; load and activate the installed packages
 
 (ensure-use-package-is-installed)
 
-; remove toolbar and menu bar
+;; remove toolbar and menu bar
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
-; enable garbage collection freeing unused memory
+;; enable garbage collection freeing unused memory
 (use-package gcmh
   :ensure t  
   :config
   (gcmh-mode 1)
-)
+  )
 
 (setup-editor-for-programming)
 
-; move save files to a central space
-; avoiding cluttering project directories
+;; move save files to a central space
+;; avoiding cluttering project directories
 (setq backup-directory-alist
       `((".*" . "~/.saves")))
 (setq auto-save-file-name-transforms
       `((".*" "~/.saves" t)))
 
-; start emacs server
+;; start emacs server
 (use-package server
   :config
-(unless (server-running-p)
-(server-start)))
+  (unless (server-running-p) ;; only start the server if it isn't already running
+    (server-start)))
 
-; Dashboard Configuration
+;; Dashboard Configuration
 (use-package dashboard
   :ensure t
   :config
@@ -81,7 +89,7 @@
 
 (install-language-server-hooks)
 
-; Setup Theme
+;; Setup Theme
 (use-package doom-themes
   :ensure t
   :config
