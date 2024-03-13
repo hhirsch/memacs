78fe3aefe98c5feaabe01967578059462697a112
Henry Hirsch <henry@w3-net.de>
2024-03-13 06:12:44 +0100

    feat: add some custom modes

--- init.el
+++ init.el
@@ -90,6 +90,17 @@
 (setq auto-save-file-name-transforms
       `((".*" "~/.saves" t)))
 
+;; Cmake support
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (use-package cmake-mode                                                ;;
+;;   :ensure t                                                            ;;
+;;   :init                                                                ;;
+;;   (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode)) ;;
+;;   (add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-mode)))        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
+
 ;; Dashboard Configuration
 (use-package dashboard
   :ensure t
@@ -101,6 +112,33 @@
   (setq dashboard-init-info "Keep things light"))
 
 (install-language-server-hooks)
+;; Go Support
+(use-package go-mode
+  :ensure t
+  :config
+  (add-hook 'go-mode-hook #'lsp-deferred)
+  (add-hook 'before-save-hook #'lsp-format-buffer))
+;; Terraform OpenTofu Support
+(use-package terraform-mode
+  :ensure t
+  :config
+  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+  (add-to-list 'auto-mode-alist '("\\.tf\\'" . terraform-mode)))
+;; Builder Mode
+(defun no-electric-indent ()
+  (setq-local electric-indent-functions '(lambda (char) 'no-indent)))
+
+(define-derived-mode builder-mode fundamental-mode "Builder Mode"
+  "Major mode for Builder scripts."
+  (setq font-lock-defaults '((builder-font-lock-keywords))))
+
+(defvar builder-font-lock-keywords
+  '(("^\\(builder-mode\\|step\\|listPackages\\|dumpPackages\\|ensurePackage\\|executeAndPrint\\)\\s-+\\(.*\\)$" 
+     (1 font-lock-keyword-face)
+     (2 font-lock-string-face))))
+(add-hook 'builder-mode-hook 'turn-on-font-lock)
+(add-hook 'builder-mode-hook 'no-electric-indent)
+(add-to-list 'auto-mode-alist '("\\.bld\\'" . builder-mode))
 
 ;; Setup Theme
 (use-package doom-themes
@@ -126,3 +164,16 @@
 
 (add-hook 'after-save-hook 'autocompile-init-file)
 (run-with-idle-timer 0 nil #'async-startup)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(terraform-mode go-mode cmake-mode use-package-el-get undo-tree php-mode nerd-icons gcmh doom-themes dashboard dap-mode company all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
